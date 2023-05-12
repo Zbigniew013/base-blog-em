4f13390d1957ae0bac9b2bdb7376f288ddec45c3
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-05-12 13:57:18 +0200

    mutation to update post title

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -33,6 +33,7 @@ export function PostDetail({ post }) {
   );
 
   const deleteMutation = useMutation(postId => deletePost(postId));
+  const updateMutation = useMutation(postId => updatePost(postId));
 
   if (isLoading) return <h1>Loading...</h1>;
   if (isError)
@@ -47,7 +48,6 @@ export function PostDetail({ post }) {
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
-      <button>Update title</button>
       {deleteMutation.isError && (
         <p style={{ color: 'red' }}>Error deleting the post</p>
       )}
@@ -56,6 +56,18 @@ export function PostDetail({ post }) {
       )}
       {deleteMutation.isSuccess && (
         <p style={{ color: 'green' }}>Post has (not) been deleted</p>
+      )}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: 'red' }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: 'purple' }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been updated</p>
       )}
       <p>{post.body}</p>
       <h4>Comments</h4>
